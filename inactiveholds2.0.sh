b44575084316bfb62e27e090fc2cf64883278f87
Andrew Nisbet <anisbet@epl.ca>
2019-01-31 17:08:40 -0700

    Added logging and tested -l.

--- inactiveholds2.0.sh
+++ inactiveholds2.0.sh
@@ -2,7 +2,8 @@
 ###########################################################################
 #
 # Bash shell script for project inactiveholds2.0
-#<one line to give the program's name and a brief idea of what it does.>
+# Builds a database on its@epl-el1.epl.ca of inactive holds.
+#
 #    Copyright (C) 2019  Andrew Nisbet, Edmonton Public Library
 # The Edmonton Public Library respectfully acknowledges that we sit on
 # Treaty 6 territory, traditional lands of First Nations and Metis people.
@@ -26,13 +27,13 @@
 SERVER=sirsi\@eplapp.library.ualberta.ca
 INACTIVE_HOLDS_DIR=/s/sirsi/Unicorn/EPLwork/cronjobscripts/Inactive_holds
 WORKING_DIR=/home/its/InactiveHolds
-VERSION="0.1"  # Dev.
+VERSION="0.2"  # Dev.
 DATABASE=inactive_holds.db
 BACKUP_DATA=inactive_holds.tar
 INACTIVE_HOLDS_TABLE_NAME=inactive_holds
 HOLD_ACTIVITY=Holds_activity_for_
 DBASE=$WORKING_DIR/$DATABASE
-VERBOSE=1
+LOG=$WORKING_DIR/inactive_holds.log
 
 # Displays the usage for this product.
 # param:  none
@@ -54,7 +55,8 @@ Flags:
       to the data directory ($WORKING_DIR/Data).
   -l: Load data from data directory ($WORKING_DIR/Data).
       The script searches for files then end in 'lst' to load,
-      then it updates the backup tar ball.
+      then it updates the backup tar ball. To improve load times the indices are removed
+      the data is loaded, then the indices are re-added.
   -L: Fetch new logs, load data, and clean files from ILS.
       Same as -f, -l, -c, in that order.
 
@@ -89,18 +91,22 @@ EOFU!
 confirm()
 {
 	if [ -z "$1" ]; then
+		echo `date +"%Y-%m-%d %H:%M:%S"`" ** error, confirm_yes requires a message." >>$LOG
 		echo "** error, confirm_yes requires a message." >&2
 		exit 1
 	fi
 	local message="$1"
+	echo `date +"%Y-%m-%d %H:%M:%S"`" $message? y/[n]: " >>$LOG
 	echo "$message? y/[n]: " >&2
 	read answer
 	case "$answer" in
 		[yY])
+			echo `date +"%Y-%m-%d %H:%M:%S"`" yes selected." >>$LOG
 			echo "yes selected." >&2
 			echo 0
 			;;
 		*)
+			echo `date +"%Y-%m-%d %H:%M:%S"`" no selected." >>$LOG
 			echo "no selected." >&2
 			echo 1
 			;;
@@ -115,12 +121,14 @@ reset_table()
     if [ -s "$DBASE" ]; then   # If the database is not empty.
         ANSWER=$(confirm "reset table $table ")
         if [ "$ANSWER" == "1" ]; then
+            echo `date +"%Y-%m-%d %H:%M:%S"`" table will be preserved. exiting" >>$LOG
             echo "table will be preserved. exiting" >&2
             exit 1
         fi
         echo "DROP TABLE $table;" | sqlite3 $DBASE 2>/dev/null
         echo 0
     else
+        echo `date +"%Y-%m-%d %H:%M:%S"`" $DBASE doesn't exist or is empty. Nothing to drop." >>$LOG
         echo "$DBASE doesn't exist or is empty. Nothing to drop." >&2
         echo 1
     fi
@@ -136,6 +144,7 @@ ensure_tables()
         # Test table(s) so we don't create tables that exist.
         ## Inactive holds table
         if echo "SELECT COUNT(*) FROM $INACTIVE_HOLDS_TABLE_NAME;" | sqlite3 $DBASE 2>/dev/null >/dev/null; then
+            echo `date +"%Y-%m-%d %H:%M:%S"`" confirmed $INACTIVE_HOLDS_TABLE_NAME exists..." >>$LOG
             echo "confirmed $INACTIVE_HOLDS_TABLE_NAME exists..." >&2
             echo 1
         else
@@ -172,9 +181,11 @@ create_database()
         echo "*warn: $DBASE exists!" >&2
         ANSWER=$(confirm "create $DBASE ")
         if [ "$ANSWER" == "0" ]; then
+            echo `date +"%Y-%m-%d %H:%M:%S"`" creating new $DBASE" >>$LOG
             echo "creating new $DBASE" >&2
             rm $DBASE
         else
+            echo `date +"%Y-%m-%d %H:%M:%S"`" Keeping database $DBASE. Exiting." >>$LOG
             echo "Keeping database $DBASE. Exiting." >&2
         fi
     else
@@ -192,6 +203,7 @@ CREATE TABLE $INACTIVE_HOLDS_TABLE_NAME (
     ItemType CHAR(20)
 );
 END_SQL
+        echo `date +"%Y-%m-%d %H:%M:%S"`" $DBASE created" >>$LOG
         echo 0
     fi
 }
@@ -211,10 +223,12 @@ CREATE INDEX IF NOT EXISTS idx_inactive_holds_date_inactive ON $INACTIVE_HOLDS_T
 CREATE INDEX IF NOT EXISTS idx_inactive_holds_date_available ON $INACTIVE_HOLDS_TABLE_NAME (DateAvailable);
 CREATE INDEX IF NOT EXISTS idx_inactive_holds_branch_date_inactive ON $INACTIVE_HOLDS_TABLE_NAME (PickupLibrary, DateInactive);
 END_SQL
+        echo `date +"%Y-%m-%d %H:%M:%S"`" indexes added created" >>$LOG
         echo 0
     else
+        echo echo `date +"%Y-%m-%d %H:%M:%S"`" **error: $DBASE doesn't exist or is empty. Use -C to create it then -l to load data from backup." >>$LOG
         echo "**error: $DBASE doesn't exist or is empty. Use -C to create it then -l to load data from backup." >&2
-        echo 1
+        exit 1
     fi
 }
 
@@ -234,10 +248,12 @@ DROP INDEX IF EXISTS idx_inactive_holds_date_inactive;
 DROP INDEX IF EXISTS idx_inactive_holds_date_available;
 DROP INDEX IF EXISTS idx_inactive_holds_branch_date_inactive;
 END_SQL
+        echo `date +"%Y-%m-%d %H:%M:%S"`" indices dropped." >>$LOG
         echo 0
     else
+        echo `date +"%Y-%m-%d %H:%M:%S"`" **error: $DBASE doesn't exist or is empty. Use -C to create it then -l to load data from backup." >>$LOG
         echo "**error: $DBASE doesn't exist or is empty. Use -C to create it then -l to load data from backup." >&2
-        echo 1
+        exit 1
     fi
 }
 
@@ -247,18 +263,25 @@ END_SQL
 load_inactive_holds()
 {
     if [ -s "$DBASE" ]; then
-        for log_list in $(ls $WORKING_DIR/Data); do
-            cat $log_list | /home/its/bin/pipe.pl -m"c0:INSERT INTO inactive\_holds (PickupLibrary\,InactiveReason\,DateInactive\,DateHoldPlaced\,HoldType\,Override\,NumberOfPickupNotices\,DateNotified\,DateAvailable\,ItemType) VALUES (\"######\",c1:\"####################\",c2:#,c3:#,c4:\"##\",c5:\"##\",c6:#,c7:#,c8:#,c9:\"####################\");" -h, >$log_list.sql
+        for log_list in $(ls $WORKING_DIR/Data/$HOLD_ACTIVITY*); do
+            cat $log_list | /home/its/bin/pipe.pl -ocontinue -m"c0:INSERT INTO inactive\_holds (PickupLibrary\,InactiveReason\,DateInactive\,DateHoldPlaced\,HoldType\,Override\,NumberOfPickupNotices\,DateNotified\,DateAvailable\,ItemType) VALUES (\"######\",c1:\"####################\",c2:#,c3:#,c4:\"##\",c5:\"##\",c6:#,c7:#,c8:#,c9:\"####################\");" -h, >$log_list.sql
             if [ -f "$log_list.sql" ]; then
+                echo `date +"%Y-%m-%d %H:%M:%S"`" loading $log_list.sql..." >>$LOG
+                echo "loading $log_list.sql..." >&2
                 cat $log_list.sql | sqlite3 $DBASE
+                rm $log_list.sql
+                echo `date +"%Y-%m-%d %H:%M:%S"`" loaded successfully." >>$LOG
+                echo "loaded successfully." >&2
             else
+                echo `date +"%Y-%m-%d %H:%M:%S"`" * warn: no records to load. $log_list.sql contains no statements." >>$LOG
                 echo "* warn: no records to load. $log_list.sql contains no statements." >&2
             fi
         done
         echo 0
     else
+        echo `date +"%Y-%m-%d %H:%M:%S"`" **error: $DBASE doesn't exist or is empty. Use -C to create it then -l to load data from backup." >>$LOG
         echo "**error: $DBASE doesn't exist or is empty. Use -C to create it then -l to load data from backup." >&2
-        echo 1
+        exit 1
     fi
 }
 
@@ -275,25 +298,31 @@ cleanup()
     if cd $WORKING_DIR/Data; then
         echo "now in "`pwd` >&2
         if tar uvf $BACKUP_DATA $HOLD_ACTIVITY*; then
+            echo `date +"%Y-%m-%d %H:%M:%S"`" '$HOLD_ACTIVITY' files successfully backed up." >>$LOG
             echo "'$HOLD_ACTIVITY' files successfully backed up." >&2
             rm $HOLD_ACTIVITY*
             ## Uncomment below after testing.
             # if ! ssh $SERVER "rm $INACTIVE_HOLDS_DIR/$HOLD_ACTIVITY*"; then
+                # echo `date +"%Y-%m-%d %H:%M:%S"`" *warn: failed to clean up '$HOLD_ACTIVITY' files from ILS." >>$LOG
                 # echo "*warn: failed to clean up '$HOLD_ACTIVITY' files from ILS." >&2
                 # echo "*warn: Do it manually to avoid duplicate inserts of data." >&2
                 # echo 1
             # fi
         else
+            echo `date +"%Y-%m-%d %H:%M:%S"`" failed to backup '$HOLD_ACTIVITY' files. Not cleaning the ILS either." >>$LOG
             echo "failed to backup '$HOLD_ACTIVITY' files. Not cleaning the ILS either." >&2
             exit 1
         fi
     else
+        echo `date +"%Y-%m-%d %H:%M:%S"`" $WORKING_DIR/Data is required but not created, exiting because nothing to clean up." >>$LOG
         echo "$WORKING_DIR/Data is required but not created, exiting because nothing to clean up." >&2
         exit 1 
     fi
     echo 0
 }
 
+# Fetches files from the ILS, creating the Data directory if required.
+# param:  none
 fetch_files()
 {
     if [ -d "$WORKING_DIR/Data" ]; then
@@ -301,10 +330,12 @@ fetch_files()
     else
         ANSWER=$(confirm "create directory $WORKING_DIR/Data ")
         if [ "$ANSWER" == "0" ]; then
+            echo `date +"%Y-%m-%d %H:%M:%S"`" creating $WORKING_DIR/Data" >>$LOG
             echo "creating $WORKING_DIR/Data" >&2
             mkdir -p $WORKING_DIR/Data
             scp $SERVER:$INACTIVE_HOLDS_DIR/*.lst $WORKING_DIR/Data
         else
+            echo `date +"%Y-%m-%d %H:%M:%S"`" $WORKING_DIR/Data is required but not created, exiting." >>$LOG
             echo "$WORKING_DIR/Data is required but not created, exiting." >&2
             exit 1
         fi
@@ -314,27 +345,34 @@ fetch_files()
 # Argument processing.
 while getopts ":cCdfilLx" opt; do
   case $opt in
-	c)	echo "-c triggered to clean up the files on the ILS ($SERVER)." >&2
+	c)	echo "-c triggered to clean up the files on the ILS ($SERVER)." >>$LOG
+        echo "-c triggered to clean up the files on the ILS ($SERVER)." >&2
         cleanup
 		;;
-    C)	echo "-C triggered to create new database (if necessary)." >&2
+    C)	echo "-C triggered to create new database (if necessary)." >>$LOG
+        echo "-C triggered to create new database (if necessary)." >&2
         create_database
 		;;
-    d)	echo "-d triggered to drop indices from $DBASE." >&2
+    d)	echo "-d triggered to drop indices from $DBASE." >>$LOG
+        echo "-d triggered to drop indices from $DBASE." >&2
         drop_indices
 		;;
-    f)	echo "-f triggered to fetch files from the ILS ($SERVER)." >&2
+    f)	echo "-f triggered to fetch files from the ILS ($SERVER)." >>$LOG
+        echo "-f triggered to fetch files from the ILS ($SERVER)." >&2
         fetch_files
 		;;
-    i)	echo "-i triggered to rebuild indices" >&2
+    i)	echo "-i triggered to rebuild indices" >>$LOG
+        echo "-i triggered to rebuild indices" >&2
         ensure_indices
 		;;
-    l)	echo "-l triggered to run data load" >&2
+    l)	echo "-l triggered to run data load" >>$LOG
+        echo "-l triggered to run data load" >&2
         drop_indices
         load_inactive_holds
         ensure_indices
 		;;
-    L)	echo "-L triggered to run" >&2
+    L)	echo "-L triggered to run" >>$LOG
+        echo "-L triggered to run" >&2
         fetch_files
         drop_indices
         load_inactive_holds
@@ -342,12 +380,6 @@ while getopts ":cCdfilLx" opt; do
         cleanup
 		;;
 	x)	usage
-		;;
-	\?)	echo "Invalid option: -$OPTARG" >&2
-		usage
-		;;
-	:)	echo "Option -$OPTARG requires an argument." >&2
-		usage
 		;;
   esac
 done
